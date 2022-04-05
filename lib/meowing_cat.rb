26b01d1760d1deecefdd79beda4bbb728688402f
Caleb Hamernick <caleb.hamernick@gmail.com>
2022-04-05 14:50:47 -0400

    commit tree f2f8bb1b65ab2eaa4fff6bc12de59c13a2b3d777

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
 ## code your solution here. 
+class Cat
+    
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+
+end
+
+boots = Cat.new
+
+boots.name = "Boots"
+
+boots.name
